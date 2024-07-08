bb09ce46dc4cd62f5221ee8fe663fc9b15480562
Guibsb90 <122750380+Guibsb90@users.noreply.github.com>
2024-07-08 15:15:30 -0300

    ajustes e retirada do codigo ajax redundante

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-const endPoint = 'https://api.github.com/users/Guibsb90' ;
+const endPoint = 'https://api.github.com/users/Guibsb90';
 
-$.ajax(endPoint).done(function(resposta){
-  const nameElement = document.querySelector('.profile-name');
-  const usernameElement = document.querySelector('.profile-username');
-  const avatarElement = document.querySelector('.profile-avatar');
-  const reposElement = document.querySelector('#numeroRepositorios');
-  const followersElement = document.querySelector('#numeroSeguidores');
-  const followingElement = document.querySelector('#numeroSeguindo');
-  const linkelement = document.querySelector('.profile-link');
-
-  fetch('https://api.github.com/users/Guibsb90').then(function(res){
+fetch(endPoint)
+  .then(function(res) {
+    if (!res.ok) {
+      throw new Error('Network response was not ok ' + res.statusText);
+    }
     return res.json();
   })
-  .then(function(json){
+  .then(function(json) {
+    const nameElement = document.querySelector('.profile-name');
+    const usernameElement = document.querySelector('.profile-username');
+    const avatarElement = document.querySelector('.profile-avatar');
+    const reposElement = document.querySelector('#numeroRepositorios');
+    const followersElement = document.querySelector('#numeroSeguidores');
+    const followingElement = document.querySelector('#numeroSeguindo');
+    const linkElement = document.querySelector('.profile-link');
+
     nameElement.innerText = json.name;
     usernameElement.innerText = json.login;
     avatarElement.src = json.avatar_url;
     followingElement.innerText = json.following;
     followersElement.innerText = json.followers;
     reposElement.innerText = json.public_repos;
-    linkelement.href = json.html_url;
-
+    linkElement.href = json.html_url;
   })
-})
+  .catch(function(error) {
+    alert("Ocorreu um erro ao buscar o endereço.");
+    console.error('Erro:', error);
+  });
